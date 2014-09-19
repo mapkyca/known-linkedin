18238c3901186810e2a43fe970b31d1a24570344
Marcus Povey <marcus@marcus-povey.co.uk>
2014-09-19 10:29:19 +0100

    Admin save

---
+++ LinkedIn/Pages/Admin.php
@@ -0,0 +1,38 @@
+<?php
+
+    /**
+     * LinkedIn pages
+     */
+
+    namespace IdnoPlugins\LinkedIn\Pages {
+
+        /**
+         * Default class to serve LinkedIn settings in administration
+         */
+        class Admin extends \Idno\Common\Page
+        {
+
+            function getContent()
+            {
+                $this->adminGatekeeper(); // Admins only
+                $t = \Idno\Core\site()->template();
+                $body = $t->draw('admin/linkedin');
+                $t->__(['title' => 'LinkedIn', 'body' => $body])->drawPage();
+            }
+
+            function postContent() {
+                $this->adminGatekeeper(); // Admins only
+                $appId = $this->getInput('appId');
+                $secret = $this->getInput('secret');
+                \Idno\Core\site()->config->config['linkedin'] = [
+                    'appId' => $appId,
+                    'secret' => $secret
+                ];
+                \Idno\Core\site()->config()->save();
+                \Idno\Core\site()->session()->addMessage('Your LinkedIn application details were saved.');
+                $this->forward('/admin/linkedin/');
+            }
+
+        }
+
+    }
