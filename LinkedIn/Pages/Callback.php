eda9a74edb9e23a8f25c5ba1cd50d638e1e45fc6
Marcus Povey <marcus@marcus-povey.co.uk>
2015-06-15 12:15:28 +0100

    Converting linkedin to internal webservices; added extra debug

--- LinkedIn/Pages/Callback.php
+++ LinkedIn/Pages/Callback.php
@@ -23,36 +23,56 @@
 				'authorization_code',
 				['code' => $this->getInput('code'), 'redirect_uri' => \IdnoPlugins\LinkedIn\Main::getRedirectUrl(), 'state' => \IdnoPlugins\LinkedIn\Main::getState()])
 			    ) {
-
-				// Catch some errors
-				if (($this->getInput('error')) && ($error = $this->getInput('error_description')))
-					throw new \Exception($error);	
+				/** Catch various errors */
+				if ($response->error) {
+				    throw new \Exception($response->error_description);
+				}
+				
+				// Verify access token
+				if (!$response->access_token) {
+				    throw new \Exception('Sorry, access token is unavailable.');
+				}
+				
+				if (($this->getInput('error')) && ($error = $this->getInput('error_description'))) {
+				    throw new \Exception($error);	
+				}
+				/**/
 
 				$user  = \Idno\Core\site()->session()->currentUser();
 
-				$basic_profile = $linkedinAPI->fetch('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,site-standard-profile-request)', array('oauth2_access_token' => $response['result']['access_token'], 'format' => 'json'));
+				$result = \Idno\Core\Webservice::get(
+				    'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,site-standard-profile-request)', 
+				    [
+					'oauth2_access_token' => $response->access_token, 
+					'format' => 'json'
+				    ]
+				);
+				$basic_profile = json_decode($result['content']);
 
-				if (!$basic_profile['result']['id'])
+				if (!$basic_profile->id)
 				{
-				    if ($basic_profile['result']['message'])
-					throw new \Exception($basic_profile['result']['message']);
+				    if ($basic_profile->message)
+					throw new \Exception($basic_profile->message);
 				    else 
 					throw new \Exception("Sorry, there was a problem getting your profile. Does your app have appropriate permissions?");
 				}
 
-				$id = $basic_profile['result']['id'];
-				$name = $basic_profile['result']['firstName'] . ' ' . $basic_profile['result']['lastName'];
-				$user->linkedin[$id] = ['access_token' => $response['result']['access_token'], 'name' => $name];
+				$id = $basic_profile->id;
+				$name = $basic_profile->firstName . ' ' . $basic_profile->lastName;
+				$user->linkedin[$id] = ['access_token' => $response->access_token, 'name' => $name];
 
 				// Get company pages
 				if (\Idno\Core\site()->config()->multipleSyndicationAccounts()) {
-				    $admin_pages = $linkedinAPI->fetch('https://api.linkedin.com/v1/companies', array('oauth2_access_token' => $response['result']['access_token'], 'format' => 'json', 'is-company-admin' => 'true'));
-				    if (!empty($admin_pages['result'])) {
-					if (!empty($admin_pages['result']['values'])) {
-					    foreach($admin_pages['result']['values'] as $company) {
+				    
+				    $result = \Idno\Core\Webservice::get('https://api.linkedin.com/v1/companies', array('oauth2_access_token' => $response->access_token, 'format' => 'json', 'is-company-admin' => 'true'));
+				    $admin_pages = $result['content'];
+				    
+				    if (!empty($admin_pages)) {
+					if (!empty($admin_pages->values)) {
+					    foreach($admin_pages->values as $company) {
 						$id = $company['id'];
 						$name = $company['name'];
-						$user->linkedin[$id] = ['access_token' => $response['result']['access_token'], 'name' => $name, 'company' => true];
+						$user->linkedin[$id] = ['access_token' => $response->access_token, 'name' => $name, 'company' => true];
 					    }
 					}
 				    }
